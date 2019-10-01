b135d1394819d9b500a8da8ee1268edcaaeced8f
dutkiewiczmaciej <dutkiewiczmaciej@me.com>
2019-10-02 01:43:35 +0200

    add fraction function to ingredients

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 /** Global State of the App
  * - Search object
@@ -67,7 +68,8 @@ const controlRecipe = async () => {
 
     if (id) {
         // Prepare UI for changes
-
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
         // Create a new recipe object
         state.recipe = new Recipe(id);
 
@@ -79,7 +81,8 @@ const controlRecipe = async () => {
             state.recipe.calcTime();
             state.recipe.calcServings();
             // Render recipe
-            console.log(state.recipe);
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
         } catch (error) {
             alert('Error with processing recipe!')
         }
--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -33,6 +33,7 @@ export default class Recipe {
     parseIngredients() {
         const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
         const unitShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+        const units = [...unitShort, 'kg', 'g'];
  
         const newIngredients = this.ingredients.map(el => {
             // 1) Uniform units
@@ -44,7 +45,7 @@ export default class Recipe {
             ingredient = ingredient.replace(/ *\([^)]*\) */g, ' ');
             // 3) Parse ingredients into count, unit an ingredient itself
             const arrIng = ingredient.split(' ');
-            const unitIndex = arrIng.findIndex(el2 => unitShort.includes(el2));
+            const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
 
             let objIng;
             
