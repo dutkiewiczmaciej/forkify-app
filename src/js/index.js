039dbb760180e8795cd509fe2c4af60db16e0984
dutkiewiczmaciej <dutkiewiczmaciej@me.com>
2019-10-03 01:01:56 +0200

    add localStorage functionality for keeping liked recipes

--- src/js/index.js
+++ src/js/index.js
@@ -140,9 +140,7 @@ elements.shopping.addEventListener('click', e => {
 /**
  * LIKE CONTROLLER
  */
-// TESTING
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
+
 
 const controlLike = () => {
     if (!state.likes) state.likes = new Likes();
@@ -175,6 +173,21 @@ const controlLike = () => {
     }
     likesView.toggleLikeMenu(state.likes.getNumLikes());
 };
+
+// Restore liked recipes on Page Load
+
+window.addEventListener('load', () => {
+    state.likes = new Likes();
+
+    // Restore likes
+    state.likes.readStorage();
+    
+    // Toggle button like menu
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+    // Render the existing likes 
+    state.likes.likes.forEach(like => likesView.renderLike(like));
+});
 
 
 // Handling recipe button clicks
