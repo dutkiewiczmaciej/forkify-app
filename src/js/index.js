77b51cd17d9341f48ad7807e999ffaac23c47a18
dutkiewiczmaciej <dutkiewiczmaciej@me.com>
2019-10-02 04:02:45 +0200

    add List.js, add methods to addItem, deleteItem, updateCount on the list

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
@@ -100,12 +101,17 @@ const controlRecipe = async () => {
 
 // Handling recipe button clicks
 elements.recipe.addEventListener('click', e => {
-    if (e.target.matches('.btn-decrease, btn-decrease *')) {
+    if (e.target.matches('.btn-decrease, .btn-decrease *')) {
         // Decrease button is clicked
-        state.recipe.updateServings('dec');
-    } else if (e.target.matches('.btn-increase, btn-increase *')) {
+        if (state.recipe.servings > 1) {
+            state.recipe.updateServings('dec');
+            recipeView.updateServingsIngredients(state.recipe);
+        }
+    } else if (e.target.matches('.btn-increase, .btn-increase *')) {
         // Increase button is clicked
         state.recipe.updateServings('inc');
+        recipeView.updateServingsIngredients(state.recipe);
     }
-    console.log(state.recipe);
 });
+
+window.l = new List();
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -116,3 +116,13 @@ export const renderRecipe = recipe => {
 
     elements.recipe.insertAdjacentHTML('afterbegin', markup);
 };
+
+export const updateServingsIngredients = recipe => {
+    // Update the servings
+    document.querySelector('.recipe__info-data--people').textContent = recipe.servings;
+    // Update the ingredients
+    const countElements = Array.from(document.querySelectorAll('.recipe__count'));
+    countElements.forEach((el, i)=> {
+        el.textContent = formatCount(recipe.ingredients[i].count)
+    });
+}
